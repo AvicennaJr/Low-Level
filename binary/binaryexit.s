a92ff8aadc4c66e1d6e731fd1aaf2223e524ce5a
AvicennaJr <justfuad@protonmail.com>
2023-01-20 21:52:43 +0300

    Add linker output files

--- binary/binaryexit.s
+++
@@ -1,6 +0,0 @@
-.section .text
-.globl _start
-_start:
-	movq $0b1101, %rdi
-	movq $60, %rax
-	syscall
--- valuesize/valuesize.s
+++
@@ -1,9 +0,0 @@
-.section .text
-.globl _start
-_start:
-	movw $0b0000000100000010, %bx
-	add %bh, %bl
-	movb $0, %bh
-	movq %rbx, %rdi
-	movq $60, %rax
-	syscall
